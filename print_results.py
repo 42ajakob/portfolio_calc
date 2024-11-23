49cf7d85e6a1dfa846f6f57d943f926817c2a31f
42ajakob <ajakob@student.42heilbronn.de>
2024-11-23 18:57:01 +0100

    changed total_interest format to human readable

--- print_results.py
+++ print_results.py
@@ -1,5 +1,5 @@
 def print_results(total_interest, etfs_weight, annual_rate):
 	for i in range(len(etfs_weight)):
 		print(f"ETF[{i + 1}] weight: {etfs_weight[i] * 100:.2f}%")
-	print(f"Total interest of weight: {total_interest:.2f}%")
+	print(f"Total interest of weight: {total_interest * 100:.2f}%")
 	print(f"Annual rate: {annual_rate:.2f}%")
