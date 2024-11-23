a51e883275430823847de42498bdceaea17023f3
42ajakob <ajakob@student.42heilbronn.de>
2024-11-23 23:29:09 +0100

    Takes a file as input && Each ETF now has it's own period of time adjustable && Calculates the weight based on the annual rate of every ETF instead of total rate now

--- main.py
+++ main.py
@@ -1,21 +1,66 @@
+import sys
 from calc import *
 from print_results import *
 
-def main():
-	print("Portfolio Weight Calculator")
+def calc_and_print(etfs_return, start_date, end_date):
+	loop = len(etfs_return)
+	annual_rate = []
 
-	loop = get_int("Number of Index Funds [1-100]: ")
-	etfs_return = alloc_float_arr(loop)
 	for i in range(loop):
-		etfs_return[i] = get_float(i)
-	start_date = get_date("Enter the start date [DD.MM.YY]: ")
-	end_date = get_date("Enter the end date [DD.MM.YY]: ")
+		annual_rate.append(calc_annual_rate(start_date[i], end_date[i], etfs_return[i]))
 
-	etfs_weight = calc_weight(etfs_return, loop)
-	total_interest = calc_total_interest(etfs_return, etfs_weight)
-	annual_rate = calc_annual_rate(start_date, end_date, total_interest)
+	etfs_weight = calc_weight(annual_rate, loop)
+	total_interest = calc_total_interest(annual_rate, etfs_weight)
 
 	print_results(total_interest, etfs_weight, annual_rate)
+
+	etfs_return.clear()
+	start_date.clear()
+	end_date.clear()
+	annual_rate.clear()
+
+def read_file(file_path):
+	etfs_return = []
+	start_date_list = []
+	end_date_list = []
+
+	with open(file_path, 'r') as file:
+		print("[Portfolio Weight Calculator]")
+		for line in file:
+			if line.strip() == '':
+				calc_and_print(etfs_return, start_date_list, end_date_list)
+				print()
+				continue
+
+			parts = line.strip().split('|')
+
+			if any(char.isalpha() for char in parts[0].strip()):
+				print(line.strip())
+				continue
+
+			value = float(parts[0].strip()) / 100
+			start_date = datetime.strptime(parts[1].strip(), '%d.%m.%y').date()
+			end_date = datetime.strptime(parts[2].strip(), '%d.%m.%y').date()
+
+			etfs_return.append(value)
+			start_date_list.append(start_date)
+			end_date_list.append(end_date)
+
+	calc_and_print(etfs_return, start_date_list, end_date_list)
+
+def main():
+
+	if len(sys.argv) != 2:
+		print("Usage: python main.py portfolio_example.txt")
+		sys.exit(1)
+
+	file_path = sys.argv[1]
+	try:
+		
+		read_file(file_path)
+	except:
+		print("Error! File not found or wrong input")
+		print("Try: python main.py portfolio_example.txt")
 
 if __name__ == "__main__":
 	main()
