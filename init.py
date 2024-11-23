183807a90e058d072b0141576faf8c9f3da3a255
42ajakob <ajakob@student.42heilbronn.de>
2024-11-23 18:07:22 +0100

    Added error handling && get_date returns date now instead of date_str

--- init.py
+++ init.py
@@ -1,23 +1,31 @@
+import sys
+from datetime import datetime
+
 def alloc_float_arr(size):
 	return [0.0] * size
 
 def get_int(msg):
 	while True:
 		try:
-			return int(input(msg))
+			user_in = int(input(msg))
+			if user_in < 1 or user_in > 100:
+				continue
+			return user_in
 		except ValueError:
 			continue
 
 def get_float(i):
 	while True:
 		try:
-			return float(input(f"ETF[{i + 1}] total return: "))
+			user_in = float(input(f"ETF[{i + 1}] total return: "))
+			if -sys.float_info.max <= user_in <= sys.float_info.max:
+				return user_in
 		except ValueError:
 			continue
 
 def get_date(msg):
 	while True:
 		try:
-			return input(msg)
+			return datetime.strptime(input(msg), '%d.%m.%y')
 		except ValueError:
 			continue
