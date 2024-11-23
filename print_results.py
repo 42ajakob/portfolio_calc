6dfeeecf232ae6e8a195c595553306690f34239f
42ajakob <ajakob@student.42heilbronn.de>
2024-11-23 23:09:01 +0100

    changed annual_rate to a array

--- print_results.py
+++ print_results.py
@@ -2,4 +2,6 @@ def print_results(total_interest, etfs_weight, annual_rate):
 	for i in range(len(etfs_weight)):
 		print(f"ETF[{i + 1}] weight: {etfs_weight[i] * 100:.2f}%")
 	print(f"Total interest of weight: {total_interest * 100:.2f}%")
-	print(f"Annual rate: {annual_rate * 100:.2f}%")
+	for i in range(len(annual_rate)):
+		print(f"Annual rate: {annual_rate[i] * 100:.2f}%")
+	print()
