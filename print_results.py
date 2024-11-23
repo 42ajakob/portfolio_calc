b5b2b985ad3daae5936134bc18763cb411767b5c
42ajakob <ajakob@student.42heilbronn.de>
2024-11-23 23:27:40 +0100

    Easier readability

--- print_results.py
+++ print_results.py
@@ -1,7 +1,5 @@
 def print_results(total_interest, etfs_weight, annual_rate):
 	for i in range(len(etfs_weight)):
-		print(f"ETF[{i + 1}] weight: {etfs_weight[i] * 100:.2f}%")
-	print(f"Total interest of weight: {total_interest * 100:.2f}%")
-	for i in range(len(annual_rate)):
-		print(f"Annual rate: {annual_rate[i] * 100:.2f}%")
-	print()
+		print(f"ETF[{i + 1}] weight: {etfs_weight[i] * 100:.2f}% | Annual rate: {annual_rate[i] * 100:.2f}%")
+	if etfs_weight:
+		print(f"Annual rate of combined ETFs weight: {total_interest * 100:.2f}%")
