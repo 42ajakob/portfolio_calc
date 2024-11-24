f7e7c77a79b8d8a9c3565aa4b1101daeab67cf7b
42ajakob <ajakob@student.42heilbronn.de>
2024-11-24 16:49:50 +0100

    Improved precision of annual return per ETF

--- calc.py
+++ calc.py
@@ -36,7 +36,7 @@ def precise_calc(etfs_return, start_date, end_date):
 	total_interest = calc_total_interest(etfs_return, weight)
 	ann_rate_interest = calc_ann_rate(start_date[0], end_date[0], total_interest)
 	for i in range(loop):
-		ann_rate_weight.append(ann_rate[i] * weight[i])
+		ann_rate_weight.append(etfs_return[i] * weight[i] * ann_rate[i])
 
 	print_results(weight, ann_rate_interest, ann_rate, ann_rate_weight)
 
