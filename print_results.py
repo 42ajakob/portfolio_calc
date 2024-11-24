1fe6a132c19b4909723f8802dc51c49e904815ea
42ajakob <ajakob@student.42heilbronn.de>
2024-11-24 01:08:49 +0100

    removed space

--- print_results.py
+++ print_results.py
@@ -5,6 +5,5 @@ def print_results(total_interest, etfs_weight, annual_rate):
 		print(f"Annual rate of combined ETFs weight based on each ETFs annual rates: {total_interest * 100:.2f}%")
 
 def print_res(total_annual_rate_precise):
-	print()
 	print("WARNING! This one is ONLY correct if every ETF has the same starting date and end date!")
 	print(f"Annual rate of combined ETFs weight based on each ETFs total return: {total_annual_rate_precise * 100:.2f}%")
