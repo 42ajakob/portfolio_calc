3a94609f6776283503278bf15267ecfea2a34aa9
42ajakob <ajakob@student.42heilbronn.de>
2024-12-07 01:15:23 +0100

    Fixed weight distribution into annual rate.
    Unfortunatly it comes with a precision lose duo to fixed number calculations and/or because of floats/doubles limits

--- calc.py
+++ calc.py
@@ -36,7 +36,7 @@ def precise_calc(etfs_return, start_date, end_date):
 	total_interest = calc_total_interest(etfs_return, weight)
 	ann_rate_interest = calc_ann_rate(start_date[0], end_date[0], total_interest)
 	for i in range(loop):
-		ann_rate_weight.append(ann_rate_interest * weight[i])
+		ann_rate_weight.append(ann_rate[i] * weight[i])
 
 	print_results(weight, total_interest, ann_rate_interest, ann_rate, ann_rate_weight)
 
@@ -52,7 +52,7 @@ def unprecise_calc(etfs_return, start_date, end_date):
 	total_interest = calc_total_interest(etfs_return, weight)
 	ann_rate_interest = calc_total_interest(ann_rate, weight)
 	for i in range(loop):
-		ann_rate_weight.append(ann_rate_interest * weight[i])
+		ann_rate_weight.append(ann_rate[i] * weight[i])
 
 	print_warning()
 	print_results(weight, total_interest, ann_rate_interest, ann_rate, ann_rate_weight)
@@ -68,7 +68,7 @@ def precise_calc_with_weight(etfs_return, start_date, end_date, weight):
 	total_interest = calc_total_interest(etfs_return, weight)
 	ann_rate_interest = calc_ann_rate(start_date[0], end_date[0], total_interest)
 	for i in range(loop):
-		ann_rate_weight.append(ann_rate_interest * weight[i])
+		ann_rate_weight.append(ann_rate[i] * weight[i])
 
 	print_results(weight, total_interest, ann_rate_interest, ann_rate, ann_rate_weight)
 
@@ -83,7 +83,7 @@ def unprecise_calc_with_weight(etfs_return, start_date, end_date, weight):
 	total_interest = calc_total_interest(etfs_return, weight)
 	ann_rate_interest = calc_total_interest(ann_rate, weight)
 	for i in range(loop):
-		ann_rate_weight.append(ann_rate_interest * weight[i])
+		ann_rate_weight.append(ann_rate[i] * weight[i])
 
 	print_warning()
 	print_results(weight, total_interest, ann_rate_interest, ann_rate, ann_rate_weight)
